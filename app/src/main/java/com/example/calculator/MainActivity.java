27bc9323ece53924bb8d78fd334479b560d07ca2
Jacob Sung <jacobcs@illinois.edu>
2020-05-04 12:11:19 -0500

    Added Documentation for the project

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -8,20 +8,32 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
+
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Arrays;
 
+/**
+ * Main Activity that handles the creation of buttons as well as functionality of the Statistics Calculator
+ * The calculator handles the calculations of Mean, Standard Deviation, Median, and Variance while also utilizing SoundPool to play sounds when buttons are pressed
+ */
 public class MainActivity extends AppCompatActivity {
+    /**
+     * Instance variables for buttons used on the calculator
+     */
     Button num0, num1, num2, num3, num4, num5, num6, num7,
             num8, num9, mean, variance, stdev, median, decimal, comma, clear;
+    /**
+     * Initializes an Edit Text object to show user the inputted text for the Calculator
+     */
     EditText editor;
-    private double population;
-    private double value2;
+    /**
+     * Initializes a SoundPool object to handle the implementation of sound effects
+     */
     private SoundPool soundPool;
     private int fartSound;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,9 +57,9 @@ public class MainActivity extends AppCompatActivity {
         comma = findViewById(R.id.comma);
         clear = findViewById(R.id.clear);
         editor = findViewById(R.id.editor);
-        //idk. what is textview vs edittext wtf
 
 
+        // Button Creation for the calculator
         num1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -59,72 +71,84 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 editor.setText(editor.getText() + "2");
+                soundPool.play(fartSound, 1, 1, 0, 0, 1);
             }
         });
         num3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 editor.setText(editor.getText() + "3");
+                soundPool.play(fartSound, 1, 1, 0, 0, 1);
             }
         });
         num4.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 editor.setText(editor.getText() + "4");
+                soundPool.play(fartSound, 1, 1, 0, 0, 1);
             }
         });
         num5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 editor.setText(editor.getText() + "5");
+                soundPool.play(fartSound, 1, 1, 0, 0, 1);
             }
         });
         num6.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 editor.setText(editor.getText() + "6");
+                soundPool.play(fartSound, 1, 1, 0, 0, 1);
             }
         });
         num7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 editor.setText(editor.getText() + "7");
+                soundPool.play(fartSound, 1, 1, 0, 0, 1);
             }
         });
         num8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 editor.setText(editor.getText() + "8");
+                soundPool.play(fartSound, 1, 1, 0, 0, 1);
             }
         });
         num9.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 editor.setText(editor.getText() + "9");
+                soundPool.play(fartSound, 1, 1, 0, 0, 1);
             }
         });
         decimal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 editor.setText(editor.getText() + ".");
+                soundPool.play(fartSound, 1, 1, 0, 0, 1);
             }
         });
         comma.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 editor.setText(editor.getText() + ",");
+                soundPool.play(fartSound, 1, 1, 0, 0, 1);
             }
         });
         num0.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 editor.setText(editor.getText() + "0");
+                soundPool.play(fartSound, 1, 1, 0, 0, 1);
             }
         });
         clear.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 editor.setText(editor.getText() + "");
+                soundPool.play(fartSound, 1, 1, 0, 0, 1);
             }
         });
 
@@ -132,33 +156,43 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 editor.setText(Double.toString(mean(buttonArray())));
+                soundPool.play(fartSound, 1, 1, 0, 0, 1);
             }
         });
         median.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 editor.setText(Double.toString(median(buttonArray())));
+                soundPool.play(fartSound, 1, 1, 0, 0, 1);
             }
         });
         stdev.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 editor.setText(Double.toString(stdev(buttonArray())));
+                soundPool.play(fartSound, 1, 1, 0, 0, 1);
             }
         });
         variance.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 editor.setText(Double.toString(variance(buttonArray())));
+                soundPool.play(fartSound, 1, 1, 0, 0, 1);
             }
         });
         clear.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v) {
                 editor.setText("");
+                soundPool.play(fartSound, 1, 1, 0, 0, 1);
             }
         });
-        //Using SoundPool to implement sounds
+        /**
+         * Using SoundPool to implement sounds. Initially it checks the version of Android in which the application is running on
+         * It then establishes the sound attributes using the AudioAttributes class
+         * We then set attributes to our SoundPool object for it to build/handle the usage of sound effects
+         */
+
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             AudioAttributes audioAttributes = new AudioAttributes.Builder()
@@ -173,10 +207,16 @@ public class MainActivity extends AppCompatActivity {
         } else {
             soundPool = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
         }
+        // Fart sound sound effect, located in the raw folder within the project
         fartSound = soundPool.load(this, R.raw.fart, 1);
 
 
     }
+
+    /**
+     * Handles the user inputs by the buttons and displays the inputted commands through text.
+     * @return data - A string array containing the commands the user inputs through the calculator
+     */
     public double[] buttonArray() {
         String stringData = editor.getText().toString();
         String[] splitData = stringData.split(",");
@@ -187,6 +227,11 @@ public class MainActivity extends AppCompatActivity {
         return data;
     }
 
+    /**
+     * Handles the actual calculation of the mean of inputted numbers
+     * @param array - the array containing the values of the inputted user sample by the calculator
+     * @return The calculation of the mean
+     */
     public double mean(double[] array) {
         double sum = 0;
         for (int i = 0; i < array.length; i++) {
@@ -194,6 +239,12 @@ public class MainActivity extends AppCompatActivity {
         }
         return sum / (array.length);
     }
+
+    /**
+     * Handles the calculation of the variance if inputted numbers
+     * @param array the array containing the values of the inputted user sample by the calculator
+     * @return The calculation of the variance
+     */
     public double findVariance(double[] array) {
         double mean = mean(array);
         double sum = 0;
@@ -205,13 +256,31 @@ public class MainActivity extends AppCompatActivity {
         }
         return sum / array.length;
     }
+
+    /**
+     * Handles the calculation of the standard deviation of the inputted user sample
+     * @param array the array containing the values of the inputted user sample by the calculator
+     * @return The Standard Deviation of the sample
+     */
     public double stdev(double[] array) {
         double variance = findVariance(array);
         return Math.sqrt(variance);
     }
+
+    /**
+     * Returns the variance using the variance helper function
+     * @param array the array containing the values of the inputted user sample by the calculator
+     * @return The variance of the user inputted sample
+     */
     public double variance(double[] array) {
         return findVariance(array);
     }
+
+    /**
+     * calculates the median of the user inputted sample
+     * @param array the array containing the values of the inputted user sample by the calculator
+     * @return The median of the user inputted sample
+     */
     public double median(double[] array) {
         double med = 0;
         Arrays.sort(array);
